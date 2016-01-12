9f11fd7f835bd137c879d33463e3e9de355c7f85
GeethChadalawada <GeethChadalawada@missionrnd.com>
2016-01-12 13:07:38 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -18,9 +18,34 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 */
 
 #include <stddef.h>
+#include <string.h>
+#include <ctype.h>
 
+void count_vowels_and_consonants(char *str, int *consonants, int *vowels)
+{
+	int i = 0, alphaCount = 0, len = 0, vowCount = 0;
 
-void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
+	if (str == NULL)
+	{
+		*consonants = 0;
+		*vowels = 0;
+	}
+	else
+	{
+		len = strlen(str);
 
+		for (i = 0; i < len; i++)
+		{
+			if (isalpha(str[i]))
+			{
+				alphaCount++;
 
+				if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U')
+					vowCount++;
+			}
+		}
+
+		*vowels = vowCount;
+		*consonants = alphaCount - vowCount;
+	}
 }
